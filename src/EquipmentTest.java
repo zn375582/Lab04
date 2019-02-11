8b67f50280d91592b1b378deb57f46dad800d26f
Jane Doe <nicolelzimmerer@ou.edu>
2019-02-11 17:37:43 -0600

    Fix equalsTest() method

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -26,12 +26,39 @@ public class EquipmentTest
         Assert.assertEquals(50.6, e.getTotalWeight(), 0.1);
         Assert.assertEquals(70.6, e.getTotalPrice(), 0.1);
         Assert.assertEquals("Something you sometimes just can't get rid of", e.getDescription());
+        
+        //test more Equipment objects
+        Equipment eq = new Equipment("Bat Shark Repellent/3,3.0,3.0,Shark repelling bat spray");
+        Assert.assertEquals("Bat Shark Repellent", eq.getName());
+		Assert.assertEquals(3, eq.getCount());
+        Assert.assertEquals(3.0, eq.getTotalWeight(), 0.1);
+        Assert.assertEquals(3.02, eq.getTotalPrice(), 0.1);
+        Assert.assertEquals("Shark repelling bat spray", eq.getDescription());
 		
 		
 	}
 	
 	public void equalsTest() throws AssertException
 	{
+		Equipment eq = new Equipment("Bat Shark Repellent/3,3.0,3.0,Shark repelling bat spray");
+		Equipment e = new Equipment("Bomb/1,50.6,70.6,Something you sometimes just can't get rid of");
+		Equipment equip = new Equipment("Bomb/1,50.6,70.6,Something you sometimes just can't get rid of");
+		
+		//Check to see if each field are equal
+		Assert.assertEquals(e.getName(), equip.getName());
+		Assert.assertEquals(e.getCount(), equip.getCount());
+		Assert.assertEquals(e.getTotalPrice(), equip.getTotalPrice());
+		Assert.assertEquals(e.getTotalWeight(), equip.getTotalWeight());
+		Assert.assertEquals(e.getDescription(), equip.getDescription());
+			
+		//test if Equipment objects are equal
+		boolean expected = true;
+		boolean actual = e.equals(equip);
+		Assert.assertEquals(expected, actual);
+			
+			
+		
+		
 		
 	}
 	
